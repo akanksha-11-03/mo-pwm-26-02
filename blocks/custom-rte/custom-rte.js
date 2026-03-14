0f27b64714fbc401b3f8754e285c762e371c9a81
anuragpandey718 <anurag.pandey@deptagency.com>
2026-03-14 20:39:29 +0530

    added

--- blocks/custom-rte/custom-rte.js
+++ blocks/custom-rte/custom-rte.js
@@ -26,6 +26,18 @@ export default function decorate(block) {
       el.classList.add(`wc-item-${i + 1}`);
       Array.from(el.children).forEach((ele, ind) => {
         ele.classList.add(`wc-inner-${ind + 1}`);
+        Array.from(ele.children).forEach((elee, indd) => {
+          elee.classList.add(`wc-inner-item-${indd + 1}`);
+          Array.from(elee.children).forEach((item, index) => {
+            item.classList.add(`wc-list-${index + 1}`);
+            Array.from(item.children).forEach((list, innd) => {
+              list.classList.add(`listitem-${innd + 1}`);
+              Array.from(list.children).forEach((e, indexx) => {
+                e.classList.add('inneritem', `inneritem-${indexx + 1}`);
+              });
+            });
+          });
+        });
       });
     });
   }
