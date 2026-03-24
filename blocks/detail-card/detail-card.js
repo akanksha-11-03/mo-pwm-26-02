10568999201b2090f99f1fd0a2770cc9771bd3f1
anuragpandey718 <anurag.pandey@deptagency.com>
2026-03-24 12:39:45 +0530

    added

--- blocks/detail-card/detail-card.js
+++ blocks/detail-card/detail-card.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const detailSec = block.closest('.cmmn-explr-btn');
-  Array.from(detailSec.querySelector('.default-content-wrapper').children).forEach((list) => {
+  Array.from(detailSec.querySelector('.default-content-wrapper')?.children).forEach((list) => {
     list.classList.add('button-list');
     Array.from(list.children).forEach((item, i) => {
       item.classList.add(`list-item-${i + 1}`);
@@ -27,5 +27,21 @@ export default function decorate(block) {
         });
       });
     });
+  });
+
+  const eachCard = block.querySelectorAll('.detail-content');
+  eachCard.forEach((card) => {
+    const cardFirstItem = card.querySelector('.detail-card-item-1');
+    const cardContent = cardFirstItem.querySelector('.content-1');
+    const cardLink = cardFirstItem.querySelector('a');
+    if (!cardLink) return;
+    const redirectURL = cardLink.getAttribute('href');
+    if (!redirectURL) return;
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', redirectURL);
+    anchor.classList.add('card-link');
+    anchor.appendChild(cardContent);
+    cardFirstItem.appendChild(anchor);
+    cardFirstItem.querySelector('.content-2')?.remove();
   });
 }
