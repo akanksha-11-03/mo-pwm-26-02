ed9fda2554a4f96b67c5d3edcb0630cab99732cb
anuragpandey718 <anurag.pandey@deptagency.com>
2026-03-27 16:03:50 +0530

    added

--- blocks/detail-card/detail-card.js
+++ blocks/detail-card/detail-card.js
@@ -4,6 +4,22 @@ export default function decorate(block) {
   setClassPrefixes(['detail-content', 'detail-card-item-', 'content-', 'inner-content-', 'inner-item-']);
   addIndexed(block);
 
+  // Array.from(block.children).forEach((child) => {
+  //   child.classList.add('detail-content');
+  //   Array.from(child.children).forEach((item, i) => {
+  //     item.classList.add(`detail-card-item-${i + 1}`);
+  //     Array.from(item.children).forEach((el, ind) => {
+  //       el.classList.add(`content-${ind + 1}`);
+  //       Array.from(el.children).forEach((elem, indd) => {
+  //         elem.classList.add(`inner-content-${indd + 1}`);
+  //         Array.from(elem.children).forEach((elee) => {
+  //           elee.classList.add('inner-item');
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+
   const eachCard = block.querySelectorAll('[class*="detail-content"]');
   eachCard.forEach((card) => {
     const cardFirstItem = card.querySelector('.detail-card-item-1');
