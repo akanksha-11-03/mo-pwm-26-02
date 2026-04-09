16984cc9bba9b9eff0cff1570c48fa1241f96c08
sakpal-A <akanksha.sakpal@deptagency.com>
2026-04-09 12:46:29 +0530

    delete folder

--- components/press-release/press-release.js
+++
@@ -1,8 +0,0 @@
-import { loadCSS } from '../../scripts/aem.js';
-
-export function decoratePressRelease(document) {
-  const section = document.querySelector('.press-release');
-  if (!section) return;
-
-  loadCSS(`${window.hlx.codeBasePath}/components/press-release/press-release.css`);
-}
